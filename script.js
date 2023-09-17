39b217b7e72241dbc4c6b7eeb4fbfd28777958bf
Homeria <35024139+Homeria@users.noreply.github.com>
2023-09-17 21:26:20 +0900

    2023-09-17 : Update
    1. Add Key Interval

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class db {
-  constructor(key, h, m , s, ms) {
+  constructor(key, h, m, s, ms) {
     this.key = key;
     this.h = h;
     this.m = m;
@@ -26,153 +26,180 @@ class db {
   getSec() {
     return this.s;
   }
-  
+
   getMilSec() {
     return this.ms;
   }
 
   getInfo() {
-    return ("key : " + this.key + ", time : " + this.getTime())
+    return ("key : " + this.key + ", time : " + this.getTime());
   }
 
-  getPeriod(target) {
-    let t1 = (((((this.h * 60) + this.m) * 60) + this.sec) * 60) + this.ms;
-    let t2 = (((((target.getHour() * 60) + target.getMin()) * 60) + target.getSec) * 60) + target.getMilSec;
-    return (t1 - t2);
-  }
 }
 
 let logs = [];
 
 function updateKeyList(key, time) {
-    let list = document.getElementById("key-list");
-    let item = document.createElement("li");
-    
-    let k = document.createElement("div");
-    let keySpan = document.createElement("span");
-    k.setAttribute("id", "key");
-    keySpan.setAttribute("id", "key-text");
-    keySpan.innerHTML = key;
-    
-    let t = document.createElement("div");
-    let timeSpan = document.createElement("span");
-    t.setAttribute("id", "time");
-    timeSpan.setAttribute("id", "time-text");
-    timeSpan.innerHTML = time;
+  let list = document.getElementById("key-list");
+  let item = document.createElement("li");
 
-    t.appendChild(timeSpan);
-    k.appendChild(keySpan);
-    item.appendChild(k);
-    item.appendChild(t);
-    list.appendChild(item);
+  let k = document.createElement("div");
+  let keySpan = document.createElement("span");
+  k.setAttribute("id", "key");
+  keySpan.setAttribute("id", "key-text");
+  keySpan.innerHTML = key;
 
-    const lastItem = list.lastElementChild;
-    const lastItemPosition = lastItem.offsetTop + lastItem.offsetHeight;
-    list.scrollTo(0, lastItemPosition);
+  let t = document.createElement("div");
+  let timeSpan = document.createElement("span");
+  t.setAttribute("id", "time");
+  timeSpan.setAttribute("id", "time-text");
+  timeSpan.innerHTML = time;
 
+  t.appendChild(timeSpan);
+  k.appendChild(keySpan);
+  item.appendChild(k);
+  item.appendChild(t);
+  list.appendChild(item);
+
+  const lastItem = list.lastElementChild;
+  const lastItemPosition = lastItem.offsetTop + lastItem.offsetHeight;
+  list.scrollTo(0, lastItemPosition);
+
+}
+
+function cleaningList() {
+  let list = document.getElementById("key-list");
+  removeAllChildNodes(list);
+  logs = [];
+  console.log(logs);
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function saveLogs() {
+
+  if (logs.length === 0) {
+    alert("The log not exist. You can't save!");
+    return;
   }
 
-  function cleaningList() {
-    let list = document.getElementById("key-list");
-    removeAllChildNodes(list);
-    logs = [];
-    console.log(logs);
+  let textToSave = "";
+
+  for (let i = 0; i < logs.length; i++) {
+    textToSave += logs[i].getInfo() + "\n";
   }
 
-  function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
-  }
+  let now = new Date();
 
-  function saveLogs() {
+  let filename = 'saved_log ' + getDateString(now) + 'txt';
+  download(filename, textToSave);
+}
 
-    if(logs.length === 0) {
-      alert("The log not exist. You can't save!");
-      return;
-    }
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
 
-    let textToSave = "";
-
-    for(let i = 0; i < logs.length; i++) {
-      textToSave += logs[i].getInfo() + "\n";
-    }
-
-    let now = new Date();
-
-    let filename = 'saved_log ' + getDateString(now) + 'txt';
-    download(filename, textToSave);
-  }
-
-  function download(filename, text) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
-    element.style.display = 'none';
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-  }
-
-  function getDateString(now) {
-    let s = 
+function getDateString(now) {
+  let s =
     now.getFullYear() + "-"
     + (now.getMonth() + 1) + "-"
-    + now.getDate() + " " ;
-    
-    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    s += "(" + daysOfWeek[now.getDay()] + ") "
+    + now.getDate() + " ";
 
-    s += now.getHours() + "-"
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  s += "(" + daysOfWeek[now.getDay()] + ") "
+
+  s += now.getHours() + "-"
     + now.getMinutes() + "-"
     + now.getSeconds() + "-"
     + now.getMilliseconds();
 
-    return s;
+  return s;
 
+}
+
+function getInterval() {
+  let interval;
+  if(logs.length === 1) {
+    interval = 0;
+  } else {
+    prevDB = logs[logs.length - 2];
+    targetDB = logs[logs.length - 1];
+
+    prevTime = (((((prevDB.getHour() * 60) + prevDB.getMin()) * 60) + prevDB.getSec()) * 1000) + prevDB.getMilSec();
+    targetTime = (((((targetDB.getHour() * 60) + targetDB.getMin()) * 60) + targetDB.getSec()) * 1000) +targetDB.getMilSec();
+    interval = (targetTime - prevTime) / 1000;
   }
+  return interval;
+}
 
-  // 키 입력 이벤트 리스너 등록
-  document.addEventListener("keydown", function (event) {
-    // 키 종류와 누른 시간 기록
-    var key = event.key;
-    var time = new Date();
+function clickListener() {
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  let milsec = time.getMilliseconds();
 
-    var hour = time.getHours();
-    var min = time.getMinutes();
-    var sec = time.getSeconds();
-    var milsec = time.getMilliseconds();
-    logs.push(new db(key, hour, min, sec, milsec));
-    updateKeyList(key, (hour + " : " + min + " : " + sec + " : " + milsec));
-    console.log(logs);
-  });
+  logs.push(new db("click", hour, min, sec, milsec));
+  let interval = getInterval();
 
-  window.addEventListener("keydown", function(e) {
-    if (
-        e.key === "Tab"
-        || e.key === "ArrowUp"
-        || e.key === "ArrowDown"
-        || e.key === "ArrowLeft"
-        || e.key === "ArrowRight"
-        || e.key === "Home"
-        || e.key === "End"
-        || e.key === "PageUp"
-        || e.key === "PageDown"
-        || e.key === "Space"
-        || e.key === "Enter"
-        || e.key === "F1"
-        || e.key === "F2"
-        || e.key === "F3"
-        || e.key === "F4"
-        || e.key === "F5"
-        || e.key === "F6"
-        || e.key === "F7"
-        || e.key === "F8"
-        || e.key === "F9"
-        || e.key === "F10"
-        || e.key === "F11"
-        ) {
-      e.preventDefault();
-    }
-  });
+  updateKeyList("click", (hour + " : " + min + " : " + sec + " : " + milsec + " (" + interval + " sec)"));
+  
+}
+
+// 키 입력 이벤트 리스너 등록
+document.addEventListener("keydown", function (event) {
+  // 키 종류와 누른 시간 기록
+  let key = event.key;
+  let time = new Date();
+
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  let milsec = time.getMilliseconds();
+
+  logs.push(new db(key, hour, min, sec, milsec));
+
+  let interval = getInterval();
+  updateKeyList(key, (hour + " : " + min + " : " + sec + " : " + milsec + " (" + interval + " sec)"));
+  console.log(logs);
+});
+
+window.addEventListener("keydown", function (e) {
+  if (
+    e.key === "Tab"
+    || e.key === "ArrowUp"
+    || e.key === "ArrowDown"
+    || e.key === "ArrowLeft"
+    || e.key === "ArrowRight"
+    || e.key === "Home"
+    || e.key === "End"
+    || e.key === "PageUp"
+    || e.key === "PageDown"
+    || e.key === "Space"
+    || e.key === "Enter"
+    || e.key === "F1"
+    || e.key === "F2"
+    || e.key === "F3"
+    || e.key === "F4"
+    || e.key === "F5"
+    || e.key === "F6"
+    || e.key === "F7"
+    || e.key === "F8"
+    || e.key === "F9"
+    || e.key === "F10"
+    || e.key === "F11"
+  ) {
+    e.preventDefault();
+  }
+});
 
